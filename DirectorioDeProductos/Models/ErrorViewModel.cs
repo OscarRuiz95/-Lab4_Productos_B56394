4949bd18f2015fb9f85c52b0a35528a451dd499a
Oscar Ruiz <oscarruizperez95@gmail.com>
2026-04-17 17:28:11 -0600

    Agregar archivos de proyecto.

---
+++ DirectorioDeProductos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DirectorioDeProductos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
